991a44eff15be4b4ef362287898e84be8eb8ac3d
Kamil Niegowski <kamil.niegowski01@gmail.com>
2023-06-14 23:05:15 +0200

    delete value in the users.json

--- partials/main.php
+++ partials/main.php
@@ -1,27 +1,50 @@
 <?php
     
-    $dataJSON=file_get_contents("dataset/users.json",true);
-    $decodeDataJSON=json_decode($dataJSON,true);
-    
+    $decodeDataJSON = json_decode( file_get_contents( 'dataset/users.json' ), true );
     echo '<table>';
-    echo '<thead><tr><th>Name</th><th>Username</th><th>Email</th><th>Address</th><th>Phone</th><th>Company</th></tr>';
+    echo '<thead><tr><th>Name</th><th>Username</th><th>Email</th><th>Address</th><th>Phone</th><th>Company</th><th>Actions</th></tr>';
     echo '<tbody>';
     
-    foreach ($decodeDataJSON as $usersDataJSON){
+    foreach ( $decodeDataJSON as $usersDataJSON ) {
         
-        $name=$usersDataJSON['name'];
-        $username=$usersDataJSON['username'];
-        $email=$usersDataJSON['email'];
-        $addressStreet=$usersDataJSON['address']['street'];
-        $addressZipCode=$usersDataJSON['address']['zipcode'];
-        $addressCity=$usersDataJSON['address']['city'];
-        $phone=$usersDataJSON['phone'];
-        $companyName=$usersDataJSON['company']['name'];
+        $id = $usersDataJSON[ 'id' ];
+        $name = $usersDataJSON[ 'name' ];
+        $username = $usersDataJSON[ 'username' ];
+        $email = $usersDataJSON[ 'email' ];
+        $addressStreet = $usersDataJSON[ 'address' ][ 'street' ];
+        $addressZipCode = $usersDataJSON[ 'address' ][ 'zipcode' ];
+        $addressCity = $usersDataJSON[ 'address' ][ 'city' ];
+        $phone = $usersDataJSON[ 'phone' ];
+        $companyName = $usersDataJSON[ 'company' ][ 'name' ];
         
-        echo '<tr><td>'.$name.'</td><td>'.$username.'</td><td><a href="mailto:'.$email.'">'.$email.'</a></td><td>'.$addressStreet.', '.$addressZipCode.' '.$addressCity.'</td><td>'.$phone.'</td><td>'.$companyName.'</td><td>';
-        
+        echo '<tr><td>' . $name . '</td><td>' . $username . '</td><td><a href="mailto:' . $email . '">' . $email . '</a></td>
+            <td>' . $addressStreet . ', ' . $addressZipCode . ' ' . $addressCity . '</td><td>' . $phone . '</td><td>' . $companyName .
+            '</td><td><form action="" method="post">
+            <input type="hidden" name="id" value="' . $id . '">
+            <input type="submit" name="removeUser" value="REMOVE" >
+            </form></td>';
     }
     
-    echo'</tbody>';
+    echo '</tbody>';
     echo '</table>';
-
+    
+    
+    if ( isset( $_POST[ 'removeUser' ] ) && isset( $_POST[ 'id' ] ) ) {
+        removeUser();
+        header( 'Location: ' . $_SERVER[ 'REQUEST_URI' ] );
+        exit();
+    }
+    
+    function removeUser()
+    {
+        global $decodeDataJSON;
+        $idDoUsuniecia = $_POST[ 'id' ];
+        
+        foreach ( $decodeDataJSON as $klucz => $rekord ) {
+            if ( $rekord[ 'id' ] == $idDoUsuniecia ) {
+                unset( $decodeDataJSON[ $klucz ] );
+                break;
+            }
+        }
+        file_put_contents( 'dataset/users.json', json_encode( $decodeDataJSON ) );
+    }
