4dfd2fc8bd08142c52f35d9a5575a401295fcf60
Kamil Niegowski <kamil.niegowski01@gmail.com>
2023-06-14 18:15:21 +0200

    get value from the users.json

--- partials/main.php
+++ partials/main.php
@@ -1,6 +1,27 @@
 <?php
-
-// Please add your logic here
-
-echo "<h1 class='starting-title'>Nice to see you! &#128075;</h1>";
+    
+    $dataJSON=file_get_contents("dataset/users.json",true);
+    $decodeDataJSON=json_decode($dataJSON,true);
+    
+    echo '<table>';
+    echo '<thead><tr><th>Name</th><th>Username</th><th>Email</th><th>Address</th><th>Phone</th><th>Company</th></tr>';
+    echo '<tbody>';
+    
+    foreach ($decodeDataJSON as $usersDataJSON){
+        
+        $name=$usersDataJSON['name'];
+        $username=$usersDataJSON['username'];
+        $email=$usersDataJSON['email'];
+        $addressStreet=$usersDataJSON['address']['street'];
+        $addressZipCode=$usersDataJSON['address']['zipcode'];
+        $addressCity=$usersDataJSON['address']['city'];
+        $phone=$usersDataJSON['phone'];
+        $companyName=$usersDataJSON['company']['name'];
+        
+        echo '<tr><td>'.$name.'</td><td>'.$username.'</td><td>'.$email.'</td><td>'.$addressStreet.', '.$addressZipCode.' '.$addressCity.'</td><td>'.$phone.'</td><td>'.$companyName.'</td><td>';
+        
+    }
+    
+    echo'</tbody>';
+    echo '</table>';
 
